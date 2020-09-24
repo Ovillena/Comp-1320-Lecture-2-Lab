88f494e82dc7878b1f3e9a0bc14c893ddf92e38d
Sara Wasim <sara@Saras-MacBook-Pro.local>
2020-09-23 20:01:53 -0700

    added isLeapYear function

--- lab-three.js
+++ lab-three.js
@@ -1,3 +1,28 @@
-function getDayOfTheWeek(year,month,day){
+/*
+if (year is not divisible by 4) then (it is a common year)
+else if (year is not divisible by 100) then (it is a leap year)
+else if (year is not divisible by 400) then (it is a common year)
+else (it is a leap year)
+*/
+
+function isLeapYear(year) {
+    if (((year % 4 == 0) && (year % 100 != 0)) || (year % 400 == 0)) {
+        return true
+    } else {
+        return false
+    }   
+}
+
+function getDayOfTheWeek(year, month, day) {
     
 }
+
+//test
+console.log(isLeapYear(4))
+console.log(isLeapYear(4))
+console.log(isLeapYear(8))
+console.log(isLeapYear(100))
+console.log(isLeapYear(400))
+console.log(isLeapYear(404))
+console.log(isLeapYear(104))
+console.log(isLeapYear(200))
